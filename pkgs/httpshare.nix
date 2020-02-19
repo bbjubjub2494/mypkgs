a6573f54d992017ff0ee32696b4ca7517f1f8149
Louis Bettens <louis@bettens.info>
2020-02-19 22:22:02 +0100

    bump httpshare to 1.0.8

--- pkgs/httpshare.nix
+++ pkgs/httpshare.nix
@@ -1,8 +1,10 @@
-{ callPackage }:
+{ callPackage, fetchFromGitHub }:
 let
-  source = (fetchGit {
-    url = "https://github.com/lourkeur/httpshare";
-    name = "httpshare-source";
-  }).outPath;
+  source = fetchFromGitHub {
+    owner = "lourkeur";
+    repo = "httpshare";
+    rev = "a211034e53e13df2e62e89334c26f36fff72500d";  # 1.0.8
+    sha256 = "154dkkwxqmrmn4s5mqvan6pv8ic6vrvf9vrlbal6i6bx3sc2cdf8";
+  };
 in
   callPackage (source + "/package.nix") {}
